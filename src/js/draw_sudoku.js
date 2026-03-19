1c1a5832d0545841888815169ef5dd7d2744ea4f
atleSkaanes <atle.skaanes@gmail.com>
2026-03-19 11:25:21 +0100

    Fixed region width

--- src/js/draw_sudoku.js
+++ src/js/draw_sudoku.js
@@ -3,27 +3,24 @@
 export function draw_sudoku(grid) {
     let board = document.getElementById('sudoku')
     console.log(board.className)
-    board.style = 'grid-template-columns: ' + 'auto'.repeat(grid.region_height)
-    /*
-    for (let i = 0; i < (grid.size * grid.size) / 1.5; i++) {
-        let row = Math.floor(Math.random() * grid.size)
-        let col = Math.floor(Math.random() * grid.size)
-        grid.grid[row][col].num = null
-    }
-    */
+    board.style =
+        'grid-template-columns: ' + 'auto '.repeat(grid.region_height) + ';'
+
     let regions = []
     for (let i = 0; i < grid.region_height; i++) {
         for (let j = 0; j < grid.region_width; j++) {
             let region = document.createElement('div')
             region.className = 'sudoku-region'
             region.style =
-                'grid-template-columns: ' + 'auto'.repeat(grid.region_width)
+                'grid-template-columns: ' +
+                'auto '.repeat(grid.region_width) +
+                ';'
             regions.push(region)
         }
     }
 
-    for (let r = 0; r < grid.grid.length; r++) {
-        for (let c = 0; c < grid.grid[r].length; c++) {
+    for (let r = 0; r < grid.size; r++) {
+        for (let c = 0; c < grid.size; c++) {
             let cell = document.createElement('div')
 
             cell.className = 'sudoku-cell'
