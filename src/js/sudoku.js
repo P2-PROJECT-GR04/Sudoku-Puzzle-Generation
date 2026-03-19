5cff820226aef26d4cb5101fc9261abdc190a33d
atleSkaanes <atle.skaanes@gmail.com>
2026-03-19 10:14:57 +0100

    Added the Sudoku grid and cell classes Now you can init an empty sudoku, and fill its internal grid afterwards

---
+++ src/js/sudoku.js
@@ -0,0 +1,55 @@
+export class Sudoku {
+    constructor(region_width, region_height) {
+        this.size = region_width * region_height
+        this.region_width = region_width
+        this.region_height = region_height
+
+        this.grid = []
+        for (let r = 0; r < this.size; r++) {
+            this.grid.push([])
+            for (let c = 0; c < this.size; c++) {
+                this.grid[r].push(null)
+            }
+        }
+    }
+}
+
+// Example function
+// export function make_full_grid(sudoku) {
+//     for (let r = 0; r < sudoku.size; r++) {
+//         for (let c = 0; c < sudoku.size; c++) {
+//             sudoku.grid[r][c] = new Cell(1, true)
+//         }
+//     }
+// }
+
+export class Cell {
+    constructor(
+        num,
+        is_hint = false,
+        to_html_fn = () => default_to_html(this)
+    ) {
+        this.num = num
+        this.is_hint = is_hint
+        this.is_collapsed = () => this.num != null
+
+        this.to_html = to_html_fn
+    }
+}
+function default_to_html(cell) {
+    if (cell.is_collapsed()) {
+        let span = document.createElement('span')
+
+        span.className = 'sudoku-num'
+        if (cell.is_hint) {
+            span.className += ' hint'
+        }
+
+        span.innerText = `${cell.num}`
+        return span
+    } else {
+        let span = document.createElement('span')
+        span.className = 'sudoku-num'
+        return span
+    }
+}
