9c8adc7bd130fd4b7495e3514bd6605dea98c095
atleSkaanes <atle.skaanes@gmail.com>
2026-03-24 19:25:38 +0100

    Added basic interactability Still needs a lot of work to be done correctly

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,59 @@
 import { draw_sudoku } from './draw_sudoku.js'
-import { make_full_grid, make_simple_solved_grid, Sudoku } from './sudoku.js'
+import { make_simple_solved_grid, Sudoku } from './sudoku.js'
 
+let sudoku = new Sudoku(3, 3)
 
-let grid = new Sudoku(3, 3)
-make_simple_solved_grid(grid)
-draw_sudoku(grid)
+make_simple_solved_grid(sudoku)
+
+for (let i = 0; i < 50; i++) {
+    let r = Math.floor(Math.random() * sudoku.size)
+    let c = Math.floor(Math.random() * sudoku.size)
+    sudoku.grid[r][c].is_hint = false
+}
+
+draw_sudoku(sudoku)
+
+// TODO: Make this a field of Sudoku, and make it all work without global variables.
+let marked_cell = null
+
+export function mark_cell(coord) {
+    if (marked_cell != null)
+        sudoku.mark_cell(marked_cell[0], marked_cell[1], false)
+    // sudoku.grid[marked_cell[0]][marked_cell[1]].set_marked(false)
+
+    marked_cell = coord
+    if (marked_cell != null)
+        sudoku.mark_cell(marked_cell[0], marked_cell[1], true)
+    // sudoku.grid[marked_cell[0]][marked_cell[1]].set_marked(true)
+
+    console.debug(`MARKED: ${marked_cell}`)
+
+    draw_sudoku(sudoku)
+}
+
+document.addEventListener('keydown', function (event) {
+    console.log(`GOT INPUT: ${event.key}`)
+    if (marked_cell != null) {
+        if (event.key === 'Delete' || event.key === 'Backspace') {
+            let cell = marked_cell
+            mark_cell(null)
+            set_cell(cell[0], cell[1], null)
+        } else {
+            let num = Number.parseInt(event.key)
+            if (isNaN(num) || num == 0 || num > sudoku.size) return
+
+            let cell = marked_cell
+            mark_cell(null)
+            set_cell(cell[0], cell[1], event.key)
+        }
+    }
+    draw_sudoku(sudoku)
+})
+
+function set_cell(r, c, num) {
+    if (!sudoku.grid[r][c].is_hint) {
+        console.log(`Setting (${r}, ${c}) to ${num}`)
+        sudoku.grid[r][c].num = num
+    }
+}
+
--- src/js/sudoku.js
+++ src/js/sudoku.js
@@ -11,26 +11,45 @@ export class Sudoku {
                 this.grid[r].push(null)
             }
         }
+
+        this.mark_cell = (row, col, is_marked = true) => {
+            this.is_marked = is_marked
+            this.grid[row][col].is_marked = is_marked
+
+            for (let r = 0; r < this.size; r++) {
+                for (let c = 0; c < this.size; c++) {
+                    if (
+                        this.grid[r][c].num == this.grid[row][col].num &&
+                        !(row == r && col == c)
+                    ) {
+                        this.grid[r][c].is_assoc_marked = is_marked
+                    }
+                }
+            }
+        }
     }
 }
 
 // Example function
- export function make_full_grid(sudoku) {
-     for (let r = 0; r < sudoku.size; r++) {
-         for (let c = 0; c < sudoku.size; c++) {
-             sudoku.grid[r][c] = new Cell(1, true)
-         }
-     }
- }
+export function make_full_grid(sudoku) {
+    for (let r = 0; r < sudoku.size; r++) {
+        for (let c = 0; c < sudoku.size; c++) {
+            sudoku.grid[r][c] = new Cell(1, true)
+        }
+    }
+}
 
 // Example function
- export function make_simple_solved_grid(sudoku) {
-     for (let r = 0; r < sudoku.size; r++) {
-         for (let c = 0; c < sudoku.size; c++) {
-             sudoku.grid[r][c] = new Cell((3 * ( r % 3) + Math.floor(r / 3) + c) % 9 + 1, true)
-         }
-     }
- }
+export function make_simple_solved_grid(sudoku) {
+    for (let r = 0; r < sudoku.size; r++) {
+        for (let c = 0; c < sudoku.size; c++) {
+            sudoku.grid[r][c] = new Cell(
+                ((3 * (r % 3) + Math.floor(r / 3) + c) % 9) + 1,
+                true
+            )
+        }
+    }
+}
 
 export class Cell {
     constructor(
@@ -41,24 +60,35 @@ export class Cell {
         this.num = num
         this.is_hint = is_hint
         this.is_collapsed = () => this.num != null
+        this.is_marked = false
+        // If the cell is marked by associativity
+        this.is_assoc_marked = false
+
+        this.set_marked = (is_marked) => (this.is_marked = is_marked)
 
         this.to_html = to_html_fn
     }
 }
 function default_to_html(cell) {
-    if (cell.is_collapsed()) {
-        let span = document.createElement('span')
+    let span = document.createElement('span')
 
-        span.className = 'sudoku-num'
-        if (cell.is_hint) {
-            span.className += ' hint'
-        }
+    span.className = 'sudoku-num'
 
-        span.innerText = `${cell.num}`
-        return span
-    } else {
-        let span = document.createElement('span')
-        span.className = 'sudoku-num'
-        return span
+    if (cell.is_hint) {
+        span.className += ' hint'
     }
+
+    if (cell.is_marked) {
+        span.className += ' marked'
+    }
+
+    if (cell.is_assoc_marked) {
+        span.className += ' marked-assoc'
+    }
+
+    if (cell.is_collapsed()) {
+        span.innerText = `${cell.num}`
+    }
+
+    return span
 }
