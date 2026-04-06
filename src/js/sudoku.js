b751e0f8a5849417e218e8acccd367bb96610873
atleSkaanes <atle.skaanes@gmail.com>
2026-04-06 14:02:00 +0200

    Made random solved grids This finished part of #12 This still utilizes the math equation for a solved sudoku grid. This works by randomly switching the rows and columns of the otherwise static grid. The transformation still creates a valid grid, without any need for backtracking or forward checking.

--- src/js/sudoku.js
+++ src/js/sudoku.js
@@ -15,22 +15,68 @@ export class Sudoku {
 }
 
 // Example function
- export function make_full_grid(sudoku) {
-     for (let r = 0; r < sudoku.size; r++) {
-         for (let c = 0; c < sudoku.size; c++) {
-             sudoku.grid[r][c] = new Cell(1, true)
-         }
-     }
- }
+export function make_full_grid(sudoku) {
+    for (let r = 0; r < sudoku.size; r++) {
+        for (let c = 0; c < sudoku.size; c++) {
+            sudoku.grid[r][c] = new Cell(1, true)
+        }
+    }
+}
 
 // Example function
- export function make_simple_solved_grid(sudoku) {
-     for (let r = 0; r < sudoku.size; r++) {
-         for (let c = 0; c < sudoku.size; c++) {
-             sudoku.grid[r][c] = new Cell((3 * ( r % 3) + Math.floor(r / 3) + c) % 9 + 1, true)
-         }
-     }
- }
+export function make_simple_solved_grid(sudoku) {
+    for (let r = 0; r < sudoku.size; r++) {
+        for (let c = 0; c < sudoku.size; c++) {
+            sudoku.grid[r][c] = new Cell(
+                ((3 * (r % 3) + Math.floor(r / 3) + c) % 9) + 1,
+                true
+            )
+        }
+    }
+}
+
+export function make_solved_grid(sudoku) {
+    for (let r = 0; r < sudoku.size; r++) {
+        for (let c = 0; c < sudoku.size; c++) {
+            sudoku.grid[r][c] = new Cell(
+                ((3 * (r % 3) + Math.floor(r / 3) + c) % 9) + 1,
+                true
+            )
+        }
+    }
+
+    // Switch cols n² times
+    for (let i = 0; i < sudoku.size * sudoku.size; i++) {
+        let region_idx = Math.floor(Math.random() * sudoku.region_height)
+        let from =
+            region_idx * sudoku.region_width +
+            Math.floor(Math.random() * sudoku.region_width)
+        let to =
+            region_idx * sudoku.region_width +
+            Math.floor(Math.random() * sudoku.region_width)
+
+        for (let r = 0; r < sudoku.size; r++) {
+            let tmp = sudoku.grid[r][to]
+            sudoku.grid[r][to] = sudoku.grid[r][from]
+            sudoku.grid[r][from] = tmp
+        }
+    }
+
+    // Switch rows n² times
+    for (let i = 0; i < sudoku.size * sudoku.size; i++) {
+        let region_idx = Math.floor(Math.random() * sudoku.region_width)
+        let from =
+            region_idx * sudoku.region_height +
+            Math.floor(Math.random() * sudoku.region_height)
+        let to =
+            region_idx * sudoku.region_height +
+            Math.floor(Math.random() * sudoku.region_height)
+
+        let tmp = sudoku.grid[to]
+        sudoku.grid[to] = sudoku.grid[from]
+        sudoku.grid[from] = tmp
+    }
+}
 
 export class Cell {
     constructor(
