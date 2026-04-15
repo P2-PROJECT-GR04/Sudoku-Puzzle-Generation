262fe7dcdb3497245ea20e5d3e59535766a74237
atleSkaanes <atle.skaanes@gmail.com>
2026-04-15 12:11:20 +0200

    removed code duplication Made find_candidates_for_grid use find_candidates_for_cell

--- src/js/check-hint.js
+++ src/js/check-hint.js
@@ -6,78 +6,7 @@
 export function find_candidates_for_grid(sudoku) {
     for (let r = 0; r < sudoku.size; r++) {
         for (let c = 0; c < sudoku.size; c++) {
-            if (sudoku.grid[r][c].num == null) {
-                let region_r_min =
-                    Math.floor(r / sudoku.region_height) * sudoku.region_height
-
-                let region_r_max = region_r_min + sudoku.region_height - 1
-                let region_c_min =
-                    Math.floor(c / sudoku.region_width) * sudoku.region_width
-                let region_c_max = region_c_min + sudoku.region_width - 1
-
-                let candidates = []
-                for (let s = 1; s <= sudoku.size; s++) {
-                    candidates.push(s)
-                }
-                for (let inner_r = 0; inner_r < sudoku.size; inner_r++) {
-                    //This checks the row of a cell
-                    if (
-                        candidates.indexOf(
-                            Number(sudoku.grid[inner_r][c].num)
-                        ) != -1
-                    ) {
-                        candidates.splice(
-                            candidates.indexOf(
-                                Number(sudoku.grid[inner_r][c].num)
-                            ),
-                            1
-                        )
-                    }
-                }
-                for (let inner_c = 0; inner_c < sudoku.size; inner_c++) {
-                    //This checks the colum of a cell
-                    if (
-                        candidates.indexOf(
-                            Number(sudoku.grid[r][inner_c].num)
-                        ) != -1
-                    ) {
-                        candidates.splice(
-                            candidates.indexOf(
-                                Number(sudoku.grid[r][inner_c].num)
-                            ),
-                            1
-                        )
-                    }
-                }
-                //Now checks the correct region
-                for (
-                    let inner_r = region_r_min;
-                    inner_r <= region_r_max;
-                    inner_r++
-                ) {
-                    for (
-                        let inner_c = region_c_min;
-                        inner_c <= region_c_max;
-                        inner_c++
-                    ) {
-                        if (
-                            candidates.indexOf(
-                                Number(sudoku.grid[inner_r][inner_c].num)
-                            ) != -1
-                        ) {
-                            candidates.splice(
-                                candidates.indexOf(
-                                    Number(sudoku.grid[inner_r][inner_c].num)
-                                ),
-                                1
-                            )
-                            //console.log(candidates)
-                        }
-                    }
-                }
-                sudoku.grid[r][c].candidates = [] //Reset the candidates for the cell
-                sudoku.grid[r][c].candidates = candidates //Here the candidates we've found is getting into the cell class in the sudoku
-            }
+            find_candidates_for_cell(sudoku, r, c)
         }
     }
 }
